cff5cf34dbd188094fb9b0c0504b450d8b686dd1
Mikołaj Maj <136507311+xRezek@users.noreply.github.com>
2023-11-10 11:09:28 +0100

    Add files via upload

---
+++ selectionsort.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int tab[] = { 45,76,23,456,3,65,221,46,84,10,71 };
+    int min = 0;
+
+    cout << "Before:" << endl;
+    for (int i = 0; i <sizeof(tab) / sizeof(int); i++) {
+        cout << tab[i] << " ";
+    }
+ 
+        for (int i = 0; i <= sizeof(tab) / sizeof(int); i++) {
+            min = i;
+            for (int j = i + 1; j < sizeof(tab) / sizeof(int); j++) {
+                if (tab[j] < tab[min])
+                    min = j;
+            }
+            if (i != min) {
+                swap(tab[i], tab[min]);
+            }
+        }
+
+    cout << endl << "After:" << endl;
+    for (int i = 0; i < sizeof(tab) / sizeof(int); i++) {
+        cout << tab[i] << " ";
+    }
+}
